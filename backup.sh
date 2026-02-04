c39bd823c254982ad0f15e9464718891859a2a17
François Barsotti <franc_site@yahoo.com>
2026-02-04 12:16:46 +0100

    feat: Récupération du nom de dossier pour basename

--- backup.sh
+++ backup.sh
@@ -3,5 +3,7 @@
 # Objectif : créer une sauvegarde fiable (archive .tar.gz) d’un répertoire donné,
 # en la nommant avec la date/heure, et en la stockant dans /backup (créer si besoin).
 
-SOURCE="$1" # Argument du répertoire à sauvegarder
-echo "Répertoire à sauvegarder : $SOURCE"
+# SOURCE="$1" # Argument du répertoire à sauvegarder
+# echo "Répertoire à sauvegarder : $SOURCE"
+
+FOLDER_NAME=$(basename $SOURCE) # Récupère le nom du dossier à sauvegarder (Source)
