641eebf48b28d2801c4de4968db2747b5db35a13
François Barsotti <franc_site@yahoo.com>
2026-02-04 11:26:07 +0100

    Création et permission d'exécution

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# backup.sh
+# Objectif : créer une sauvegarde fiable (archive .tar.gz) d’un répertoire donné,
+# en la nommant avec la date/heure, et en la stockant dans /backup (créer si besoin).
+
